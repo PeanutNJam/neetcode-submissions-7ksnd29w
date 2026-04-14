31f9e1f66ddc80ee9eb24302bf1e55b8c03b8c27
Yu Xuan <ngyuxuan1997@gmail.com>
2026-04-14 16:24:37 +0800

    Add: min-cost-to-connect-points - submission-2

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        adj_tree = defaultdict(list)
+        n = len(points)
+        visited = set()
+        res = 0
+
+        for i in range(n):
+            x_i, y_i = points[i]
+            for j in range(i + 1, n):
+                x_j, y_j = points[j]
+                cost = abs(x_i - x_j) + abs(y_i - y_j)
+                adj_tree[i].append([cost, j])
+                adj_tree[j].append([cost, i])
+
+        heap = []
+        heapq.heappush(heap, [0, 0])
+
+        while len(visited) < n:
+            curr_dst, idx = heapq.heappop(heap)
+
+            if idx in visited:
+                continue
+
+            res += curr_dst
+
+            for dst, i in adj_tree[idx]:
+                if i not in visited:
+                    heapq.heappush(heap, [dst, i])
+            
+            visited.add(idx)
+
+        return res
