4de622175a0180a208e6ed36689c67a5cd3c70a0
Yu Xuan <ngyuxuan1997@gmail.com>
2026-04-23 19:08:48 +0800

    Update: network-delay-time - submission-10

--- Data Structures & Algorithms/network-delay-time/submission-10.py
+++ Data Structures & Algorithms/network-delay-time/submission-10.py
@@ -1,25 +1,30 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        adj_tree = defaultdict(list)
         visited = set()
 
+        adj_tree = defaultdict(list)
+
         for u, v, t in times:
-            adj_tree[u].append((t, v))
+            adj_tree[u].append([t, v])
 
         heap = []
-        heapq.heappush(heap, (0, k))
-        t = 0
+        heapq.heappush(heap, [0, k])
+        res = 0
+
 
         while heap:
-            curr_t, curr_v = heapq.heappop(heap)
-            if curr_v in visited:
+            t, u = heapq.heappop(heap)
+            if u in visited:
                 continue
 
-            t = curr_t
-            for nei_t, nei_v in adj_tree[curr_v]:
-                if nei_v not in visited:
-                    heapq.heappush(heap, (curr_t + nei_t, nei_v))
+            visited.add(u)
+            res = t
+            
+            for new_t, new_v in adj_tree[u]:
+                if new_v not in visited:
+                    heapq.heappush(heap, [t + new_t, new_v])
 
-            visited.add(curr_v)
+            
 
-        return t if len(visited) == n else -1  
+        return res if len(visited) == n else -1
+            
