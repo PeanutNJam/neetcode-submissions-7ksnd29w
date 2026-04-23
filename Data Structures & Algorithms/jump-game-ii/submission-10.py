a125d26a05ef36fda8171a168733a5c97e353f68
Yu Xuan <ngyuxuan1997@gmail.com>
2026-04-23 21:47:37 +0800

    Add: jump-game-ii - submission-10

---
+++ Data Structures & Algorithms/jump-game-ii/submission-10.py
@@ -0,0 +1,10 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * (n + 1)
+
+        for i in range(n - 2, -1, -1):
+            add = min(n - i - 1, nums[i])
+            dp[i] = min(dp[i + 1], dp[i + add]) + 1
+
+        return dp[0]
